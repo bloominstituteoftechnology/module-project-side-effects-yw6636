aa4ebb1d86c40655dec569d46ae1b0b36973895d
Yongwoo Lee <yw6636@gmail.com>
2023-08-27 16:24:52 -0400

    Finished all of the tasks given

--- frontend/components/Comments/Comments.js
+++ frontend/components/Comments/Comments.js
@@ -4,10 +4,15 @@ import Comment from './Comment';
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
-
+  console.log(comments)
   return (
     <div className='comments-wrapper'>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {
+        comments.map(comment => 
+          <Comment key={comment.id} comment={comment}/>
+        )
+      }
     </div>
   );
 };
